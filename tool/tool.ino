73893e60fd8e7e9246a06f1959a21db81e4613bf
FJ-Rato <martinho.francisco1998@gmail.com>
2020-05-27 09:18:12 +0100

    dinamcly changes the time interval of reads based on voltage

--- tool/tool.ino
+++ tool/tool.ino
@@ -9,12 +9,11 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensor(&oneWire);
 
-//Timer
-int timer = 0;
-int rst = 1;
-
 //SD card
 const int chipSelect = 10;
+
+//Analog
+const int pin_A3 = A3;
 
 //LCD pin to Arduino
 const int pin_RS = 8; 
@@ -34,9 +33,9 @@ void setup() {
   delay(1000);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("T1:  C|T2:  C");
+  lcd.print("T1:    T2:   ");
   lcd.setCursor(0,1);
-  lcd.print("T3:  C|T4:  C");
+  lcd.print("T3:    T4:   ");
   Serial.begin(9600);
 
   if (!SD.begin(chipSelect)) //checks for the card
@@ -48,18 +47,54 @@ void setup() {
 
 }
 void loop() {
-  
+
+  int button = analogRead(0);
+  if((button > 600)&&(button < 800))
+    showpw(button);
+  boot();
   int temp[] = {read_temp(0),read_temp(1),read_temp(2),read_temp(3)};
 
   send2lcd(temp);
   
-  if (timer > rst){
-    writeSD(temp);
-    timer = 0;}
-  else{
-    timer++;
-    }
+  writeSD(temp,power(analogRead(pin_A3)));
+  
 }
+
+void boot(){
+  lcd.setCursor(0,0);
+  lcd.print("T1:");
+  lcd.setCursor(7,0);
+  lcd.print("T2:");
+  lcd.setCursor(0,1);
+  lcd.print("T3:");
+  lcd.setCursor(7,1);
+  lcd.print("T4:");
+  }
+
+void showpw(int button){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("V:");
+  while((button > 600)&&(button < 800)){
+    button = analogRead(0);
+    lcd.setCursor(3,0);
+    lcd.print(analogRead(pin_A3));
+    delay(500);
+  }
+  }
+  
+int power(int pw){
+  if(pw == 0)//the engine is off
+    delay(5000);
+
+  if((pw > 0) && (pw < 100)) //low
+    delay(1000);
+
+  if(pw > 100)//high
+    delay(500);
+    
+  return pw;
+  }
 
 int read_temp(int address){ //reads the temp by IC2
   sensor.requestTemperaturesByIndex(address);
@@ -72,25 +107,25 @@ void send2lcd(int temp[]){ //writes on the display the temps
   if(validate(temp[0]))
     lcd.print(temp[0]);
   else
-    lcd.print("DC?");
+    lcd.print("DC");
 
   lcd.setCursor(10,0);
   if(validate(temp[1]))
     lcd.print(temp[1]);
   else
-    lcd.print("DC?");
+    lcd.print("DC");
 
   lcd.setCursor(3,1);
   if(validate(temp[2]))
     lcd.print(temp[2]);
   else
-    lcd.print("DC?");
+    lcd.print("DC");
     
   lcd.setCursor(10,1);
   if(validate(temp[3]))
     lcd.print(temp[3]);
   else
-    lcd.print("DC?");
+    lcd.print("DC");
    
   }
 
@@ -101,7 +136,7 @@ bool validate(int value){ //checks if the temp os valid
     return false;
   }
   
-void writeSD(int temp[]){ //writes on the SD card the temps
+void writeSD(int temp[], int pw){ //writes on the SD card the temps
   int count = 0;
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
   if (dataFile) {
@@ -113,6 +148,9 @@ void writeSD(int temp[]){ //writes on the SD card the temps
     Serial.println("Card save");
     count++;
     }
+    dataFile.print("Power: ");
+    dataFile.print(" => ");
+    dataFile.println(pw);
     dataFile.println("===========");
     dataFile.close();
   }
